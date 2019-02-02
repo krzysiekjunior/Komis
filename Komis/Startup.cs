c4589aea9cb93bbb760237d28710c907bc260208
krzysiekjunior <krzysiek.junior@gmail.com>
2019-02-02 21:50:05 +0100

    Konfiguracja gotowa (obsługa ExceptionPage, info o statusie żądania, obsł. plików statycznych i MVC z domyślnym routingiem

--- Komis/Startup.cs
+++ Komis/Startup.cs
@@ -13,22 +13,18 @@ namespace Komis
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services)  //w tej metodzie rejestruje się usługi 
+        {                                                           //framework'u które potrzebujemy i własne usługi
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+        {          //w tej metodzie konfigurujemy potok żądań i tu dodamy komponenty oprogramowania pośredniego które potrzebujemy w naszym potoku żądań
+            app.UseDeveloperExceptionPage();        // dodaje obsł. strony ExceptionPage(błędu)  (używamy podczas pisania apki)  
+            app.UseStatusCodePages();               // info o statusie żądania
+            app.UseStaticFiles();                   // obsługa plików statycznych, np: obraz, javascript, cs
+            app.UseMvcWithDefaultRoute();           // mvc z domyślnym routingiem
+        }                                                        
     }
 }
