7700fb9257871c856894329462c40672e1c907c2
krzysiekjunior <krzysiek.junior@gmail.com>
2019-02-03 17:26:57 +0100

    Dodany do aplikacji Entity Framework Core

--- Komis/Startup.cs
+++ Komis/Startup.cs
@@ -1,17 +1,27 @@
 ﻿using Komis.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Komis
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup (IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)  //w tej metodzie rejestruje się usługi 
         {                                                           //framework'u które potrzebujemy i własne usługi
-            services.AddTransient<ISamochodRepository, MockSamochodRepository>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<ISamochodRepository, SamochodRepository>();
             services.AddMvc();                                      //i tu mamy dostęp do kontenra wstrzykiwania zależności ASP.NET Core
         }
 
